79408eca69abc31ea304683ec36de4ab198e9e0f
Mehdi Faresi <mfaresi210@gmail.com>
2022-11-27 16:29:15 +0330

    Substitute a custom User model

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Local
+    'accounts',
     'home',
 ]
 
@@ -128,3 +129,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Model
+AUTH_USER_MODEL = 'accounts.User'
