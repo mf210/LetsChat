c0d1ec3c8a2e51504a15108c07ec543e07b9b302
Mehdi Faresi <mfaresi210@gmail.com>
2022-11-30 18:30:14 +0330

    Fix user profile template and view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
+from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
 
-
-class ProfileView(LoginRequiredMixin, TemplateView):
-    template_name: str = 'accounts/profile.html'
+class ProfileView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        user = get_object_or_404(get_user_model(), username=kwargs.get('username'))
+        return render(request, 'accounts/profile.html', context={'user_object': user})
