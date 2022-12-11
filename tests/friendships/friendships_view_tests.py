7766c4908919ce858206d9ac5338cb46dbc1f593
Mehdi Faresi <mfaresi210@gmail.com>
2022-12-11 22:27:39 +0330

    Write another test for AcceptFriendRequest view

--- tests/friendships/friendships_view_tests.py
+++ tests/friendships/friendships_view_tests.py
@@ -38,3 +38,15 @@ def test_receiver_can_accept_friend_request(client: Client, django_user_model):
     assert response.status_code == HTTPStatus.OK
     assert user1.friendship.is_friend_with(user2)
 
+
+def test_only_receiver_can_accept_friend_request(client: Client, django_user_model):
+    """Only the receiver can accept the friend request"""
+    test_users = create_users(django_user_model)
+    user1, user2 = test_users['user1'], test_users['user2']
+    friend_req_obj = FriendRequest.objects.create(sender=user1, receiver=user2)
+    client.force_login(user1)
+    url = reverse('friendships:accept_friend', kwargs={'pk': friend_req_obj.pk})
+    response = client.post(url)
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert not user1.friendship.is_friend_with(user2)
+
