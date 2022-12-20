07832495ec15d0dc5af106dfa68f85793280b26c
Mehdi Faresi <mfaresi210@gmail.com>
2022-12-20 11:14:16 +0330

    Create GroupChatRoomMessage model

--- groupchats/models.py
+++ groupchats/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+
 class GroupChatRoom(models.Model):
     """Group Chat Room Model"""
     name = models.CharField(max_length=255, unique=True)
@@ -16,7 +17,6 @@ class GroupChatRoom(models.Model):
     def __str__(self):
         return self.name
 
-
     @property
     def channel_name(self):
         """
@@ -25,3 +25,18 @@ class GroupChatRoom(models.Model):
         """
         return f'GroupChatRoom-{self.id}'
 
+
+class GroupChatRoomMessage(models.Model):
+    """
+    Chat messages created by a user inside a GroupChatRoom
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(GroupChatRoom, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'{self.room}:{self.user} | {self.timestamp} => {self.content[:20]}'
+
+    class Meta:
+        ordering = ['-timestamp']
--- tests/groupchats/groupchats_model_tests.py
+++ tests/groupchats/groupchats_model_tests.py
@@ -1,17 +1,32 @@
 import pytest
 
-from groupchats.models import GroupChatRoom
+from groupchats.models import GroupChatRoom, GroupChatRoomMessage
 
 
 
 
 @pytest.mark.django_db
 def test_group_chat_room_str_method():
+    """Test GroupChatRoom __str__ method"""
     group_chat_room_obj = GroupChatRoom.objects.create(name='group_one')
     assert str(group_chat_room_obj) == 'group_one'
 
 
 @pytest.mark.django_db
 def test_group_chat_room_channel_name_property():
+    """Test GroupChatRoom channel_name property"""
     obj = GroupChatRoom.objects.create(name='group_one')
     assert obj.channel_name == f'GroupChatRoom-{obj.id}'
+
+
+def test_group_chat_room_message_str_method(django_user_model):
+    """Test GroupChatRoomMessage __str__ method"""
+    user = django_user_model.objects.create(username='user-one')
+    room_obj = GroupChatRoom.objects.create(name='group-one')
+    room_msg_obj = GroupChatRoomMessage.objects.create(
+        user=user,
+        room=room_obj,
+        content='Hello World!'
+    )
+    assert str(room_msg_obj) == f"group-one:user-one | {room_msg_obj.timestamp} => Hello World!"
+
