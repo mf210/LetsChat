3c2fe07a0a33cb8e6b4e0cfe8185878686403dd0
Mehdi Faresi <mfaresi210@gmail.com>
2022-12-25 19:43:53 +0330

    Return 403 response for not joined users to the group chat room

--- groupchats/views.py
+++ groupchats/views.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -9,7 +11,10 @@ from .models import GroupChatRoom
 class GroupChatRoomView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         room_name = kwargs.get('room_name')
-        context = {
-            'chat_room_obj': get_object_or_404(GroupChatRoom, name=room_name),
-        }
+        chat_room_obj = get_object_or_404(GroupChatRoom, name=room_name)
+        if not chat_room_obj.users.filter(pk=request.user.pk).exists():
+            # user did not join to the chat room
+            return render(request, 'groupchats/not_joined.html', status=HTTPStatus.FORBIDDEN)
+
+        context = {'chat_room_obj': chat_room_obj}
         return render(request, 'groupchats/chat_room.html', context)
--- tests/groupchats/groupchats_view_tests.py
+++ tests/groupchats/groupchats_view_tests.py
@@ -6,12 +6,40 @@ from groupchats.models import GroupChatRoom
 
 
 
-
-def test_group_chat_room_view_status_code(admin_client):
-    """Http status code should be 200 just for existing chat-rooms"""
-    url = reverse('groupchats:chat-room', kwargs={'room_name': 'public'})
+def test_group_chat_room_view_return_not_found_for_not_existing_chat_room(admin_client):
+    """
+    GroupChatRoom view return 404 (Not Found) status code for not existing chat room
+    """
+    url = reverse('groupchats:chat-room', kwargs={'room_name': 'test_room'})
     response = admin_client.get(url)
     assert response.status_code == HTTPStatus.NOT_FOUND
-    GroupChatRoom.objects.create(name='public')
-    response = admin_client.get(url)
+
+
+def test_joined_users_to_group_chat_room_gets_ok_status_code(client, django_user_model):
+    """
+    Http status code should be 200 just if group chat room is exist
+    and user is joined to the group chat room
+    """
+    user1 = django_user_model.objects.create_user(
+        username='user-one',
+        email='userone@gmail.com',
+        password='password'
+    )
+    gcr_obj = GroupChatRoom.objects.create(name='test_room')
+    gcr_obj.users.add(user1)
+    client.force_login(user1)
+    url = reverse('groupchats:chat-room', kwargs={'room_name': 'test_room'})
+    response = client.get(url)
     assert response.status_code == HTTPStatus.OK
+
+
+def test_not_joined_user_to_group_chat_room_gets_forbidden_status_code(
+    admin_client, django_user_model):
+    """
+    Get 403 (Forbidden) status code
+    for users who are not in the group_chat_room users list
+    """
+    GroupChatRoom.objects.create(name='test_room')
+    url = reverse('groupchats:chat-room', kwargs={'room_name': 'test_room'})
+    response = admin_client.get(url)
+    assert response.status_code == HTTPStatus.FORBIDDEN
