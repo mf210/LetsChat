8d4dc0ef6ea6f688e0ed59731c53201760aea135
Mehdi Faresi <mfaresi210@gmail.com>
2023-01-21 12:38:12 +0330

    Make GeneralNotificationView's queries more effeiciont

--- notifications/views.py
+++ notifications/views.py
@@ -11,7 +11,9 @@ from .models import Notification
 
 class GeneralNotificationView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        all_general_notifs = request.user.notifications.all()
+        all_general_notifs = request.user.notifications.\
+            select_related('sender', 'content_type').\
+                prefetch_related('content_object').all()
         # get earliest notification based on earliest_notif_id
         earliest_notif_id = request.GET.get('earliest_notif_id')
         try:
@@ -21,19 +23,14 @@ class GeneralNotificationView(LoginRequiredMixin, View):
             notifs_list = all_general_notifs
         # prepare data for sending
         data = []
-        for notif_obj in notifs_list[:5]:
-            content_type = notif_obj.content_type.app_labeled_name
-            if content_type == 'friendships | friendship':
-                sender = notif_obj.content_object.user
-            elif content_type == 'friendships | friend request':
-                sender = notif_obj.content_object.sender
+        for notif_obj in notifs_list[:10]:
             data.append({
                 'verb': notif_obj.verb,
                 'timestamp': notif_obj.timestamp.isoformat(),
                 'is_read': notif_obj.is_read,
-                'profile_url': sender.get_absolute_url(),
-                'image_url': sender.profile_image.url,
-                'content_type': content_type,
+                'profile_url': notif_obj.sender.get_absolute_url(),
+                'image_url': notif_obj.sender.profile_image.url,
+                'content_type': notif_obj.content_type.app_labeled_name,
                 'content_object_id': notif_obj.content_object.id,
                 'notification_id': notif_obj.id,
             })
