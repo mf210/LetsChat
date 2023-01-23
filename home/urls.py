51142e9249d4e9c08834497b48f5e88386e11d19
Mehdi Faresi <mfaresi210@gmail.com>
2023-01-23 11:12:48 +0330

    Create PublicChatRoomMessagesView

--- home/urls.py
+++ home/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
     path('chats/', views.ChatsView.as_view(), name='chats'),
+    path('public_messages/', views.PublicChatRoomMessagesView.as_view(), name='public-messages'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+import json
+
+from django.shortcuts import HttpResponse, render
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from groupchats.models import GroupChatRoom, GroupChatRoomMessage
 
 
 
@@ -25,3 +29,27 @@ class ChatsView(LoginRequiredMixin, View):
         }
         return render(request, 'home/chats.html', context)
 
+
+class PublicChatRoomMessagesView(View):
+    def get(self, request, *args, **kwargs):
+        public_chat_room_msgs = GroupChatRoom.objects.get(name='public').\
+                                messages.select_related('user')
+        # get earliest chat messages based on earliest_msg_id
+        earliest_msg_id = request.GET.get('earliest_msg_id')
+        try:
+            earliest_msg = public_chat_room_msgs.get(id=earliest_msg_id)
+            msg_list = public_chat_room_msgs.filter(timestamp__lt=earliest_msg.timestamp)
+        except (GroupChatRoomMessage.DoesNotExist, ValueError):
+            msg_list = public_chat_room_msgs
+        # prepare data for sending
+        data = []
+        for msg_obj in msg_list[:25]:
+            data.append({
+                'message': msg_obj.content,
+                'username': msg_obj.user.username,
+                'profile_image_url': msg_obj.user.profile_image.url,
+                'profile_url': msg_obj.user.get_absolute_url(),
+                'msg_timestamp': msg_obj.timestamp.ctime(),
+                'msg_id': msg_obj.id,
+            })
+        return HttpResponse(json.dumps(data), content_type="application/json")
